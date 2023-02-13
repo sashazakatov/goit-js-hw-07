153f39941f604c26c7a405a7392a8476ae958356
sashazakatov <58004989+sashazakatov@users.noreply.github.com>
2023-02-13 16:33:38 +0200

    adds a solution to the first task

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,37 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const createListItemsMarkup = (items) => {
+    return items.map(({ preview, description, original }) => {
+      return `
+    <li class="gallery__item ">
+        <a class="gallery__link" href="original">
+            <img
+            class="gallery__image" 
+            src="${preview}" 
+            alt="${description}" 
+            data-source="${original}"
+            >
+        </a>
+    </li>`; 
+    }).join('');
+}
+const closeLightBox = (lightBox) => {
+    window.addEventListener('keydown', (event) => {
+        if(event.code !== 'Escape') return; lightBox.close()
+        { once: true }
+    })
+}
 
-console.log(galleryItems);
+const galleryRef = document.querySelector('.gallery');
+galleryRef.innerHTML = createListItemsMarkup(galleryItems);
+
+galleryRef.addEventListener('click', (event) => {
+    event.preventDefault();
+    if(event.target.nodeName !== 'IMG'){
+        return;
+    }
+    const lightBox = basicLightbox.create(`
+    <img width="1280" src="${event.target.dataset.source}" alt="${event.target.alt}">`, 
+    {onShow: () => closeLightBox(lightBox)});
+    lightBox.show();
+});
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,17 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryRef = document.querySelector('.gallery');
+
+const createListItemsMarkup = (items) => {
+    return items.map(({preview, description, original}) => {
+      return `
+    <li class="gallery__item ">
+        <a class="gallery__link" href="large-image.jpg">
+            <img class="gallery__image" src="${preview}" alt="${description}" data-source="${original}">
+        </a>
+    </li>`; 
+    }).join('');
+}
+
+galleryRef.innerHTML = createListItemsMarkup(galleryItems);
